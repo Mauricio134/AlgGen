d2345ab84490e2d34f66167ceb0c3d57a4aaa0d1
Mauricio Apaza <mauricioapaza@MacBook-Pro-de-Mauricio.local>
2024-04-20 05:51:23 -0500

    Algoritmo genetico

--- main.cpp
+++ main.cpp
@@ -6,204 +6,184 @@
 
 using namespace std;
 
+int mul[] = {1,-1};
+
 class GA1{
 public:
-    vector<pair<string,int>>  valores;
-    vector<int> fit;
-    pair<int, int> limites;
-    int poblacion;
-    int generaciones;
-    float promedio;
-    float maximo;
+    vector<tuple<string,int,int>> poblacion;
+    int nPobla;
+    int tBin;
     bool minimizar;
-    int tamBinario;
-    int nSeleccion;
-    int corte;
-    
-    GA1(pair<int,int>, int, int, int, int, bool);
-    void generarPoblacion();
-    int tamBinarios();
-    string getBinario(int , int);
-    int fitness(int);
-    void getFitness();
-    void getAverMaxFitness();
-    vector<pair<string, int>> seleccion();
-    void corteMuta(vector<pair<string,int>> );
-    int reacalNumber(string );
+    pair<int,int> limites;
+
+    GA1(int, bool, pair<int,int>);
+    // Métodos
+    void genPoblacion(); // Genera una poblacion de forma aleatoria
+    int tamBinario(); // Halla la cantidad de bits maximos.
+    string getBinarios(int); // Obtener los binarios
+    int f(int); // Funcion
+    void seleccion(); // Seleccion a partir de torneo
+    int getNumber(string ); // Obtener un numero de un binario
+    void cruceMuta(); // Cruzamiento y mutación
 };
 
-GA1::GA1(pair<int,int> l, int p, int g, int c, int ns, bool m){
-    limites = l;
+GA1::GA1(int nP, bool m, pair<int,int> l){
+    nPobla = nP;
+    tBin = 0;
     minimizar = m;
-    poblacion = p;
-    generaciones = g;
-    maximo = 0.0f;
-    promedio = 0.0f;
-    tamBinario = 0;
-    corte = c;
-    nSeleccion = ns;
-    valores.resize(p);
-    fit.resize(p);
+    poblacion.resize(nP);
+    limites = l;
 }
 
-void GA1::generarPoblacion(){
-    srand((unsigned) time(NULL));
-    tamBinario = tamBinarios();
-    for(int i = 0; i < poblacion; i++){
-        valores[i].second = limites.first + (rand() % (limites.second - limites.first+1));
-        valores[i].first = getBinario(valores[i].second, tamBinario);
-    }
-}
-
-int GA1::tamBinarios(){
-    int counter = 0;
+int GA1::tamBinario(){
+    int cant = 0;
     int number = limites.second;
     while(number){
-        counter++;
         number/=2;
+        cant++;
     }
-    counter++;
-    return counter;
+    cant++;
+    return cant;
 }
 
-string GA1::getBinario(int number, int tam){
-    string bin;
+string GA1::getBinarios(int number){
+    string binario = "";
     while(number){
-        bin = to_string(number%2) + bin;
-        number/=2;
-    }
-    int t = bin.size();
-    if(t < tam){
-        for(int i = t; i < tam; i++){
-            bin = "0"+bin;
+        if(number%2 == 0){
+            binario = '0' + binario;
         }
+        else if(number%2 == 1){
+            binario = '1' + binario;
+        }
+        number /= 2;
     }
-    return bin;
+    int tBinario = binario.size();
+    tBin = tamBinario();
+    for(int i = tBinario; i < tBin-1; i++){
+        binario = '0' + binario;
+    }
+    return binario;
 }
 
-int GA1::fitness(int x){
+void GA1::genPoblacion(){
+    srand(time(NULL));
+    for(int i = 0; i < nPobla; i++){
+        int num = limites.first + rand() % (limites.second -limites.first + 1);
+        string binario = getBinarios(num);
+        int fit = (mul[minimizar])*(f(num));
+        poblacion[i] = make_tuple(binario,num,fit);
+    }
+    return;
+}
+
+int GA1::f(int x){
     return pow(x,2);
-    // return x*sin(x) + pow(x,2)*cos(x);
 }
 
-void GA1::getFitness(){
-    for(int i = 0; i < poblacion; i++){
-        fit[i] = fitness(valores[i].second);
+void GA1::seleccion(){
+    vector<tuple<string,int,int>> new_poblacion;
+    for(int i = 0; i < nPobla; i++){
+        vector<int> indices;
+        if(nPobla >= 4){
+            while(indices.size()<nPobla){
+                int valor = rand()%nPobla;
+                if(find(indices.begin(), indices.end(), valor) == indices.end()){
+                    indices.push_back(valor);
+                }
+            }
+        }
+        else for(int i = 0; i < nPobla; i++) indices.push_back(rand()%nPobla);
+
+        // indices.push_back(rand()%nPobla);
+
+        // for(int i = 0; i < nPobla; i++) cout << indices[i] << " ";
+        // cout << endl;
+        tuple<string,int,int> semi1 = (get<2>(poblacion[indices[0]]) < get<2>(poblacion[indices[1]])) ? poblacion[indices[1]] : poblacion[indices[0]];
+        tuple<string,int,int> semi2 = (get<2>(poblacion[indices[2]]) < get<2>(poblacion[indices[3]])) ? poblacion[indices[3]] : poblacion[indices[2]];
+        tuple<string,int,int> fin = (get<2>(semi1) < get<2>(semi2)) ? semi2 : semi1;
+
+        new_poblacion.push_back(fin);
     }
+    poblacion = new_poblacion;
     return;
 }
 
-void GA1::getAverMaxFitness(){
-    maximo = 0.0f;
-    for(int i = 0; i < poblacion; i++){
-        promedio += (float)fit[i];
-        maximo = max((float)fit[i], maximo);
+int GA1::getNumber(string b){
+    int result = 0;
+    for(int i = b.size()-1, j = 0; i > -1; i--, j++){
+        if(b[i] == '1') result += pow(2,j);
     }
-    promedio = (float)promedio/(float)poblacion;
-    return;
-}
-
-vector<pair<string, int>> GA1::seleccion() {
-    if(!minimizar) sort(valores.begin(), valores.end(), [this](const pair<string,int> & p1, const pair<string, int> & p2){
-        return (float)this->fitness(p1.second) > (float)this->fitness(p2.second);
-    });
-    else sort(valores.begin(), valores.end(), [this](const pair<string,int> & p1, const pair<string, int> & p2){
-        return (float)this->fitness(p1.second) < (float)this->fitness(p2.second);
-    });
-    vector<pair<string,int>> result(valores.begin(), valores.begin()+ nSeleccion);
     return result;
 }
 
-int GA1::reacalNumber(string binario){
-    int suma = 0;
-    for(int i = tamBinario-1, j = 0; i > -1; i--, j++) if(binario[i] != '0') suma += pow(2,j);
-    return suma;
-}
-
-void GA1::corteMuta(vector<pair<string,int>> sel){
-    vector<pair<string, int>> nValores;
-    
-    int indMuta = rand() % poblacion;
-    for(int i = 0; i < poblacion-1; i++){
-        pair<string,string> padre;
+void GA1::cruceMuta(){
+    vector<tuple<string, int,int>> nuevoPobla;
+    int indMuta = rand() % nPobla;
+    for(int i = 0; i < nPobla; i++){
+        tuple<string,int,int> padre1, padre2;
         string hijo;
-        int i1 = rand() % nSeleccion;
-        int i2 = rand() % nSeleccion;
-        while(i2 == i1){
-            i2 = rand() % nSeleccion;
-        }
-        padre = make_pair(sel[i1].first, sel[i2].first);
+        int indice1 = rand()%nPobla;
+        int indice2 = rand()%nPobla;
+        int corte = 2 + rand()%(tBin-2);
+        // int corte = 2;
+        while(indice1 == indice2) indice2 = rand()%nPobla;
+        padre1 = poblacion[indice1];
+        padre2 = poblacion[indice2];
 
-        hijo += padre.first.substr(0, corte);
-        hijo += padre.second.substr(corte);
-
+        hijo = get<0>(padre1).substr(0,corte);
+        hijo += get<0>(padre2).substr(corte);
         if(i == indMuta){
-            // cout << "Yo mute: " << endl;
-            int bit = rand()%tamBinario;
-            if(hijo[bit] == '0') hijo[bit] = '1';
-            else hijo[bit] = '0';
+            int bit = rand()%tBin;
+            if(hijo[bit] == '1') hijo[bit] = '0';
+            else hijo[bit] = '1';
         }
-        int number = reacalNumber(hijo);
-        while(number < limites.first || limites.second < number){
+        int valorHijo = getNumber(hijo);
+        while(valorHijo < limites.first || valorHijo > limites.second){
             hijo = "";
-            i1 = rand() % nSeleccion;
-            i2 = rand() % nSeleccion;
-            while(i2 == i1){
-                i2 = rand() % nSeleccion;
-            }
-            padre = make_pair(sel[i1].first, sel[i2].first);
+            indice1 = rand()%nPobla;
+            indice2 = rand()%nPobla;
+            while(indice1 == indice2) indice2 = rand()%nPobla;
+            padre1 = poblacion[indice1];
+            padre2 = poblacion[indice2];
 
-            hijo += padre.first.substr(0, corte);
-            hijo += padre.second.substr(corte);
-
+            hijo = get<0>(padre1).substr(0,corte);
+            hijo += get<0>(padre2).substr(corte);
             if(i == indMuta){
-                // cout << "Yo mute: " << endl;
-                int bit = rand()%tamBinario;
-                if(hijo[bit] == '0') hijo[bit] = '1';
-                else hijo[bit] = '0';
+                int bit = rand()%tBin;
+                if(hijo[bit] == '1') hijo[bit] = '0';
+                else hijo[bit] = '1';
             }
-            number = reacalNumber(hijo);
+            valorHijo = getNumber(hijo);
         }
-        nValores.push_back(make_pair(hijo,number));
+        nuevoPobla.push_back(make_tuple(hijo, valorHijo, (mul[minimizar]) * this->f(valorHijo)));
     }
-    valores = nValores;
+    poblacion = nuevoPobla;
 }
 int main(){
-    GA1 algoritmo(
-        make_pair(0,31),
-        4,
-        5,
-        2,
-        3,
-        true
-    );
-    algoritmo.generarPoblacion();
-    algoritmo.getFitness();
-    algoritmo.getAverMaxFitness();
+    GA1 algo(7,false, make_pair(5,31));
 
-    cout << "Promedio: " << algoritmo.promedio << endl;
-    cout << "Maximo: " << algoritmo.maximo << endl;
-    for(int k = 0; k < algoritmo.generaciones; k++){
-        // for(int i = 0; i < algoritmo.poblacion; i++){
-        //     cout << algoritmo.valores[i].first << " :";
-        //     cout << algoritmo.valores[i].second << endl;
+    algo.genPoblacion();
+    for(int i = 0; i < algo.nPobla; i++){
+        cout << get<0>(algo.poblacion[i]) << ": ";
+        cout << get<1>(algo.poblacion[i]) << " -> ";
+        cout << get<2>(algo.poblacion[i]) << endl;
+    }
+    for(int i = 0;i < 20; i++){
+        cout << endl;
+        algo.seleccion();
+        // for(int i = 0; i < algo.nPobla; i++){
+        //     cout << get<0>(algo.poblacion[i]) << ": ";
+        //     cout << get<1>(algo.poblacion[i]) << " -> ";
+        //     cout << get<2>(algo.poblacion[i]) << endl;
         // }
-
-        vector<pair<string, int>> sel = algoritmo.seleccion();
-        // for(int i = 0; i < sel.size(); i++){
-        //     cout << sel[i].first << " :";
-        //     cout << sel[i].second << endl;
-        // }
-
-        algoritmo.corteMuta(sel);
-        algoritmo.getFitness();
-        algoritmo.getAverMaxFitness();
-        // for(int i = 0; i < algoritmo.poblacion; i++){
-        //     cout << algoritmo.valores[i].first << " :";
-        //     cout << algoritmo.valores[i].second << endl;
-        // }
-        cout << "Promedio: " << algoritmo.promedio << endl;
-        cout << "Maximo: " << algoritmo.maximo << endl;
+        cout << endl;
+        algo.cruceMuta();
+        for(int i = 0; i < algo.nPobla; i++){
+            cout << get<0>(algo.poblacion[i]) << ": ";
+            cout << get<1>(algo.poblacion[i]) << " -> ";
+            cout << get<2>(algo.poblacion[i]) << endl;
+        }
+        
     }
     return 0;
 }
